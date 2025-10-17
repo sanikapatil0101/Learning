d3379f15aedd0cad566315e4093cfbc3d3b2e043
sanikapatil0101 <sanikaspatil112006@gmail.com>
2025-10-18 00:13:12 +0530

    tailwind

---
+++ tailwind/my-project/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className=' bg-blue-300 '>hi
+      </div>
+      <br/>
+      <div className='flex justify-between'>
+  <div className='bg-blue-400 flex-1 m-1'>child1</div>
+  <div className='bg-blue-500 flex-1 m-1'>child2</div>
+  <div className='bg-blue-600 flex-1 m-1'>child3</div>
+</div>
+
+    </>
+  )
+}
+
+export default App
