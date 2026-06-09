c5868e30b88e83be42cbb6ca9e98a402ef3f11db
sanikapatil0101 <sanikaspatil112006@gmail.com>
2026-06-09 23:21:33 +0530

    websocket

---
+++ websocket/dist/index.js
@@ -0,0 +1,12 @@
+import { WebSocketServer } from 'ws';
+const wss = new WebSocketServer({ port: 8080 });
+wss.on("connection", function (socket) {
+    console.log("user connected");
+    setInterval(() => {
+        socket.send("price is " + Math.random());
+    }, 500);
+    socket.on("message", (e) => {
+        console.log(e.toString());
+    });
+});
+//# sourceMappingURL=index.js.map
